d84be90a843d4dd6e11d9766caad7ddb02359674
Dmitri Gornakov <helikeel@gmail.com>
2024-07-25 01:02:51 +0300

    gift form front end design commit

--- components/Gift.vue
+++ components/Gift.vue
@@ -15,11 +15,16 @@
       <!-- Message -->
       <Message v-if="message" :message="message" />
     </form>
-    <div v-if="certificate">
-      <h3>Ваш сертификат</h3>
-      <p>Сумма: {{ certificate.amount }} рублей</p>
-      <p>Для: {{ certificate.friendName }}</p>
-      <p>Код: {{ certificate.code }}</p>
+    <div v-if="certificate" class="certificate">
+      <!-- Отображение сертификата -->
+      <!-- <h3>Cертификат</h3> -->
+      <div class="certificate-content">
+        <p class="certificate-amount">Сумма: {{ certificate.amount }} euro</p>
+        <p class="certificate-name">Для: {{ certificate.friendName }}</p>
+        <p class="certificate-code">Код: {{ certificate.code }}</p>
+
+      </div>
+      
     </div>
 
     <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
@@ -36,10 +41,8 @@ export default {
   data() {
     return {
       message: null,
-      certificate: {
-        amount: null,
-        friendName: '',
-      },
+      amount: null,
+      friendName: '',
       certificate: null
     };
   },
@@ -63,14 +66,92 @@ export default {
 </script>
 
 <style >
+
 .gift-certificate {
-  max-width: 400px;
+  max-width: 800px;
   margin: auto;
   padding: 20px;
   border: 1px solid #ddd;
   border-radius: 5px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Центрирование по горизонтали */
+  text-align: center; /* Центрирование текста */
 }
+
+.certificate {
+  max-width: 400px;
+  margin-top: 20px;
+  padding: 120px;
+  border: 2px solid #4CAF50;
+  border-radius: 10px;
+  background: #fff;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  font-family: 'Times New Roman', Times, serif;
+  position: relative;
+  background-image: url('@/assets/img/certificate_1.jpg'); /* Путь к вашему изображению */
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.certificate h3 {
+  margin-bottom: 20px;
+  font-size: 24px;
+  font-weight: bold;
+  color: #3182ce;
+}
+
+.certificate-content {
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  padding: 10px 0;
+  margin: 20px 0;
+}
+
+.certificate-amount, .certificate-code {
+  font-size: 18px;
+  margin: 10px 0;
+}
+
+.certificate-name {
+  font-size: 10px;
+  margin: 10px 0;
+}
+
+.certificate-amount {
+  font-weight: bold;
+}
+
+.certificate-code {
+  background: #f9f9f9;
+  border: 1px dashed #4CAF50;
+  padding: 5px;
+  border-radius: 5px;
+  display: inline-block;
+}
+
+form button:hover {
+  background-color: #41de73;
+}
+
+form div {
+  margin-bottom: 10px;
+}
+
+.sp-btn {
+  background-color: #11b445;
+  margin-left: 1rem;
+  color: #fff;
+  padding: 0.5rem 1rem;
+  text-decoration: none;
+  border-radius: 0.25rem;
+  transition: background-color 0.3s;
+}
+
 
 form button:hover {
   background-color: #41de73;
