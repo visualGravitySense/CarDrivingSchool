00f6ce22e7917b2eab4445c2b01a046f51d8b261
Dmitri Gornakov <helikeel@gmail.com>
2024-07-28 23:01:30 +0300

    front end work, design commit

--- components/system/Footer.vue
+++ components/system/Footer.vue
@@ -1,214 +1,168 @@
 <template>
-    <header>
-      <div class="navbar">
-        <div class="container">
-          <div class="navbar-content">
-            <div class="logo">
-              <nuxt-link class="flex pt-8 sm:pt-0" to="/">
-                <svg width="100" height="45" viewBox="0 0 159 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M55.5017 6.81866H60.1727L70.0719 22.9912V6.81866H74.3837V29.7345H69.7446L59.8135 13.5955V29.7345H55.5017V6.81866Z" fill="#003543" /> <path d="M93.657 29.7344H89.6389V27.1747C88.7241 28.9761 86.8628 29.9904 84.5113 29.9904C80.7869 29.9904 78.3684 27.3059 78.3684 23.4423V13.2339H82.3865V22.5976C82.3865 24.8566 83.7594 26.4276 85.8171 26.4276C88.0712 26.4276 89.6389 24.6598 89.6389 22.2377V13.2339H93.657V29.7344Z" fill="#003543" /> <path d="M107.64 29.7344L103.784 24.2342L99.9291 29.7344H95.6492L101.596 21.1242L96.1074 13.2339H100.485L103.784 17.9821L107.051 13.2339H111.461L105.94 21.1242L111.886 29.7344H107.64Z" fill="#003543" /> <path d="M120.053 8.25848V13.2339H124.627V16.6063H120.053V24.7974C120.053 25.0725 120.162 25.3363 120.356 25.531C120.55 25.7257 120.813 25.8353 121.087 25.8357H124.627V29.728H121.98C118.386 29.728 116.035 27.6323 116.035 23.9687V16.6095H112.801V13.2339H114.83C115.776 13.2339 116.327 12.6692 116.327 11.7349V8.25848H120.053Z" fill="#003543" /> <path d="M134.756 24.5446V6.81866H139.066V23.1864C139.066 27.6067 136.943 29.7345 133.349 29.7345H128.332V25.8421H133.461C133.804 25.8421 134.134 25.7054 134.377 25.4621C134.619 25.2188 134.756 24.8888 134.756 24.5446Z" fill="#003543" /> <path d="M141.649 22.0409H145.799C146.029 24.6006 147.728 26.2308 150.472 26.2308C152.923 26.2308 154.623 25.2501 154.623 23.2199C154.623 18.3085 142.331 21.7129 142.331 12.9395C142.334 9.17515 145.568 6.55945 150.215 6.55945C155.05 6.55945 158.317 9.34153 158.516 13.6306H154.388C154.193 11.6341 152.632 10.2918 150.207 10.2918C147.953 10.2918 146.548 11.3397 146.548 12.9427C146.548 18.0173 159 14.2226 159 23.1576C159 27.4131 155.504 30 150.474 30C145.279 30 141.882 26.8563 141.654 22.0441" fill="#003543" /> <path d="M24.7203 29.704H41.1008C41.6211 29.7041 42.1322 29.5669 42.5828 29.3061C43.0334 29.0454 43.4075 28.6704 43.6675 28.2188C43.9275 27.7672 44.0643 27.2549 44.0641 26.7335C44.0639 26.2121 43.9266 25.6999 43.6662 25.2485L32.6655 6.15312C32.4055 5.70162 32.0315 5.32667 31.581 5.06598C31.1305 4.8053 30.6195 4.66805 30.0994 4.66805C29.5792 4.66805 29.0682 4.8053 28.6177 5.06598C28.1672 5.32667 27.7932 5.70162 27.5332 6.15312L24.7203 11.039L19.2208 1.48485C18.9606 1.03338 18.5864 0.658493 18.1358 0.397853C17.6852 0.137213 17.1741 0 16.6538 0C16.1336 0 15.6225 0.137213 15.1719 0.397853C14.7213 0.658493 14.3471 1.03338 14.0868 1.48485L0.397874 25.2485C0.137452 25.6999 0.000226653 26.2121 2.8053e-07 26.7335C-0.000226092 27.2549 0.136554 27.7672 0.396584 28.2188C0.656614 28.6704 1.03072 29.0454 1.48129 29.3061C1.93185 29.5669 2.44298 29.7041 2.96326 29.704H13.2456C17.3195 29.704 20.3239 27.9106 22.3912 24.4118L27.4102 15.7008L30.0986 11.039L38.1667 25.0422H27.4102L24.7203 29.704ZM13.0779 25.0374L5.9022 25.0358L16.6586 6.36589L22.0257 15.7008L18.4322 21.9401C17.0593 24.2103 15.4996 25.0374 13.0779 25.0374Z" fill="#00DC82" /></svg>
-              </nuxt-link>
-            </div>
-            <div class="menu" :class="{ 'menu-open': isMenuOpen }">
-              <ul class="navbar-list">
-                <li class="navbar-item" v-for="link in links" :key="link.title">
-                  <nuxt-link class="navbar-link" :title="link.title" :to="link.url">{{ link.title }}</nuxt-link>
-                </li>
-              </ul>
-              <div class="cta-buttons">
-                <nuxt-link to="/signin" class="signin-button">Log in</nuxt-link>
-                <nuxt-link to="/signup" class="signup-button">Sign up</nuxt-link>
-              </div>
-            </div>
-            <div class="burger-menu" @click="toggleMenu">
-              <span class="burger-bar"></span>
-              <span class="burger-bar"></span>
-              <span class="burger-bar"></span>
-            </div>
+  <footer class="footer">
+    <div class="footer__container">
+      
+
+      <div class="footer__row">
+        <div class="footer__column">
+          <div class="footer__logo">
+            <nuxt-link class="flex pt-8 sm:pt-0" to="/">
+              <svg width="100" height="45" viewBox="0 0 159 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M55.5017 6.81866H60.1727L70.0719 22.9912V6.81866H74.3837V29.7345H69.7446L59.8135 13.5955V29.7345H55.5017V6.81866Z" fill="#003543" /> <path d="M93.657 29.7344H89.6389V27.1747C88.7241 28.9761 86.8628 29.9904 84.5113 29.9904C80.7869 29.9904 78.3684 27.3059 78.3684 23.4423V13.2339H82.3865V22.5976C82.3865 24.8566 83.7594 26.4276 85.8171 26.4276C88.0712 26.4276 89.6389 24.6598 89.6389 22.2377V13.2339H93.657V29.7344Z" fill="#003543" /> <path d="M107.64 29.7344L103.784 24.2342L99.9291 29.7344H95.6492L101.596 21.1242L96.1074 13.2339H100.485L103.784 17.9821L107.051 13.2339H111.461L105.94 21.1242L111.886 29.7344H107.64Z" fill="#003543" /> <path d="M120.053 8.25848V13.2339H124.627V16.6063H120.053V24.7974C120.053 25.0725 120.162 25.3363 120.356 25.531C120.55 25.7257 120.813 25.8353 121.087 25.8357H124.627V29.728H121.98C118.386 29.728 116.035 27.6323 116.035 23.9687V16.6095H112.801V13.2339H114.83C115.776 13.2339 116.327 12.6692 116.327 11.7349V8.25848H120.053Z" fill="#003543" /> <path d="M134.756 24.5446V6.81866H139.066V23.1864C139.066 27.6067 136.943 29.7345 133.349 29.7345H128.332V25.8421H133.461C133.804 25.8421 134.134 25.7054 134.377 25.4621C134.619 25.2188 134.756 24.8888 134.756 24.5446Z" fill="#003543" /> <path d="M141.649 22.0409H145.799C146.029 24.6006 147.728 26.2308 150.472 26.2308C152.923 26.2308 154.623 25.2501 154.623 23.2199C154.623 18.3085 142.331 21.7129 142.331 12.9395C142.334 9.17515 145.568 6.55945 150.215 6.55945C155.05 6.55945 158.317 9.34153 158.516 13.6306H154.388C154.193 11.6341 152.632 10.2918 150.207 10.2918C147.953 10.2918 146.548 11.3397 146.548 12.9427C146.548 18.0173 159 14.2226 159 23.1576C159 27.4131 155.504 30 150.474 30C145.279 30 141.882 26.8563 141.654 22.0441" fill="#003543" /> <path d="M24.7203 29.704H41.1008C41.6211 29.7041 42.1322 29.5669 42.5828 29.3061C43.0334 29.0454 43.4075 28.6704 43.6675 28.2188C43.9275 27.7672 44.0643 27.2549 44.0641 26.7335C44.0639 26.2121 43.9266 25.6999 43.6662 25.2485L32.6655 6.15312C32.4055 5.70162 32.0315 5.32667 31.581 5.06598C31.1305 4.8053 30.6195 4.66805 30.0994 4.66805C29.5792 4.66805 29.0682 4.8053 28.6177 5.06598C28.1672 5.32667 27.7932 5.70162 27.5332 6.15312L24.7203 11.039L19.2208 1.48485C18.9606 1.03338 18.5864 0.658493 18.1358 0.397853C17.6852 0.137213 17.1741 0 16.6538 0C16.1336 0 15.6225 0.137213 15.1719 0.397853C14.7213 0.658493 14.3471 1.03338 14.0868 1.48485L0.397874 25.2485C0.137452 25.6999 0.000226653 26.2121 2.8053e-07 26.7335C-0.000226092 27.2549 0.136554 27.7672 0.396584 28.2188C0.656614 28.6704 1.03072 29.0454 1.48129 29.3061C1.93185 29.5669 2.44298 29.7041 2.96326 29.704H13.2456C17.3195 29.704 20.3239 27.9106 22.3912 24.4118L27.4102 15.7008L30.0986 11.039L38.1667 25.0422H27.4102L24.7203 29.704ZM13.0779 25.0374L5.9022 25.0358L16.6586 6.36589L22.0257 15.7008L18.4322 21.9401C17.0593 24.2103 15.4996 25.0374 13.0779 25.0374Z" fill="#00DC82" /></svg>
+            </nuxt-link>    
+            <p class="footer__description">Автошкола Viktorija: Обучение, которое ведет к мастерству!</p>
           </div>
         </div>
+        <div class="footer__column">
+          
+          <div class="footer__contact">
+  <p><strong>Адрес:</strong> <a href="https://www.google.com/maps/place/Jaama+1a,+N%C3%B5mme,+Tallinn" target="_blank" rel="noopener noreferrer">Jaama 1a (2 этаж), Nõmme, Tallinn</a></p>
+  <p><strong>Телефон:</strong> <a href="tel:+37253464508">+372 53464508</a></p>
+  <p><strong>Email:</strong> <a href="mailto:info@viktorijaautokool.ee">info@viktorijaautokool.ee</a></p>
+</div>
+
+          
+        </div>
       </div>
-    </header>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        links: [
-  
-          { title: 'About', url: '/about' },
-          { title: 'Services', url: '/services' },
-          { title: 'Reviews', url: '/reviews' },
-          { title: 'Faq', url: '/faq' },
-          // { title: 'Promotions', url: '/promotions' },
-          { title: 'Contact', url: '/contact' },
-        ],
-        isMenuOpen: false
-      };
-    },
-    methods: {
-      toggleMenu() {
-        this.isMenuOpen = !this.isMenuOpen;
-      }
-    }
-  };
-  </script>
-  
-  <style >
-  .navbar {
-    background-color: #f8f9fa;
-    border-bottom: 1px solid #e9ecef;
-    padding: 1rem 0;
-  }
-  
-  .container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  
-  .logo {
-    margin-left: 1rem;
-  }
-  
-  .logo a {
-    color: #333;
-    font-size: 1.5rem;
-    font-weight: bold;
-    text-decoration: none;
-  }
-  
-  .navbar-content {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    justify-content: space-between;
-  }
-  
-  .menu {
-    display: flex;
-    align-items: center;
-  }
-  
-  .navbar-list {
-    display: flex;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-  }
-  
-  .navbar-item {
-    margin-left: 1rem;
-  }
-  
-  .navbar-link {
-    color: #333;
-    text-decoration: none;
-    padding: 0.5rem 1rem;
-    transition: color 0.3s;
-  }
-  
-  .navbar-link:hover,
-  .navbar-link.nuxt-link-exact-active {
-    color: #4165de;
-  }
-  
-  .cta-buttons {
-    display: flex;
-    align-items: center;
-  }
-  
-  
-  .signin-button {
-    background-color: #4165de;
-    color: #fff;
-    padding: 0.5rem 1rem;
-    text-decoration: none;
-    border-radius: 0.25rem;
-  
-    transition: background-color 0.3s;
-  }
-  
-  .signup-button {
-    background-color: #41de73;
-    margin-left: 1rem;
-    color: #fff;
-    padding: 0.5rem 1rem;
-    text-decoration: none;
-    border-radius: 0.25rem;
-  
-    transition: background-color 0.3s;
-  }
-  
-  .signup-button:hover,
-  .signin-button:hover {
-    background-color: #3652b5;
-  }
-  
-  .burger-menu {
-    display: none;
-    flex-direction: column;
-    cursor: pointer;
-  }
-  
-  .burger-bar {
-    width: 25px;
-    height: 3px;
-    background-color: #333;
-    margin: 4px 0;
-    transition: 0.4s;
-  }
-  
-  @media (max-width: 768px) {
-    .menu {
-      display: none;
-      flex-direction: column;
-      align-items: flex-start;
-    }
-  
-    .menu.menu-open {
-      display: flex;
-      position: absolute;
-      top: 60px;
-      left: 0;
-      background-color: #f8f9fa;
-      width: 100%;
-      padding: 1rem;
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    }
-  
-    .navbar-list {
-      flex-direction: column;
-      width: 100%;
-    }
-  
-    .navbar-item {
-      margin-left: 0;
-      width: 100%;
-    }
-  
-    .navbar-link {
-      padding: 1rem;
-      width: 100%;
-      text-align: left;
-    }
-  
-    .cta-buttons {
-      flex-direction: column;
-      width: 100%;
-    }
-  
-    .signup-button,
-    .signin-button {
-      margin-left: 0;
-      margin-top: 1rem;
-      width: 100%;
-      text-align: center;
-    }
-  
-    .burger-menu {
-      display: flex;
+
+      <div class="footer__nav">
+            <ul class="footer__nav-list">
+              <li class="footer__nav-item"><NuxtLink to="/">Главная</NuxtLink></li>
+              <li class="footer__nav-item"><NuxtLink to="/about">О нас</NuxtLink></li>
+              <li class="footer__nav-item"><NuxtLink to="/services">Услуги</NuxtLink></li>
+              <li class="footer__nav-item"><NuxtLink to="/reviews">Отзывы</NuxtLink></li>
+              <li class="footer__nav-item"><NuxtLink to="/contact">Контакты</NuxtLink></li>
+            </ul>
+          </div>
+
+      
+
+      <div class="footer__social">
+        <a href="https://facebook.com" class="footer__social-link" target="_blank" rel="noopener noreferrer">
+          <i class="fab fa-facebook-f">Facebook</i>
+        </a>
+        <a href="https://youtube.com" class="footer__social-link" target="_blank" rel="noopener noreferrer">
+          <i class="fab fa-youtube">Youtube</i>
+        </a>
+        <a href="https://instagram.com" class="footer__social-link" target="_blank" rel="noopener noreferrer">
+          <i class="fab fa-instagram">Instagram</i>
+        </a>
+      </div>
+
+      
+    </div>
+
+    <div class="footer__bottom">
+      <p>&copy; 2024 Автошкола Viktorija. Все права защищены.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: ''
+    };
+  },
+  methods: {
+    subscribe() {
+      // Логика подписки на новости
+      alert(`Вы подписались на новости с email: ${this.email}`);
     }
   }
-  </style>
+};
+</script>
+
+<style scoped>
+
+.footer__contact a {
+  color: #fff;
+  text-decoration: none;
+}
+.footer__contact a:hover {
+  text-decoration: underline;
+}
+
+.footer {
+  background-color: #333;
+  color: #fff;
+  padding: 20px 0;
+}
+.footer__container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+
+.footer__row {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+
+.footer__column {
+  flex: 1 1 45%;
+  margin-bottom: 20px;
+}
+
+.footer__logo {
   
+}
+.footer__logo-img {
+  max-width: 150px;
+}
+.footer__description {
+  margin-top: 10px;
+  font-size: 14px;
+}
+.footer__nav {
+  margin-top: 20px;
+}
+.footer__nav-list {
+  list-style: none;
+  padding: 0;
+}
+.footer__nav-item {
+  display: inline;
+  margin: 0 10px;
+} 
+.footer__nav-item a {
+  color: #fff;
+  text-decoration: none;
+}
+.footer__contact {
+  margin-top: 20px;
+}
+.footer__social {
+  margin-top: 20px;
+}
+.footer__social-link {
+  color: #fff;
+  margin: 0 10px;
+  font-size: 20px;
+}
+.footer__subscribe {
+  margin-top: 20px;
+}
+.footer__subscribe input {
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+}
+.footer__subscribe button {
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #555;
+  color: #fff;
+  cursor: pointer;
+}
+.footer__bottom {
+  margin-top: 20px;
+  
+}
+</style>
